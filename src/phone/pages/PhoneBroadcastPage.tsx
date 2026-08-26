ef84e9f4a5c0d066cdc2b00057066abd3ba37f57
MaiTroll2025 <ceo@maitroll.com>
2026-08-26 11:11:43 -0600

    update2

--- src/phone/pages/PhoneBroadcastPage.tsx
+++ src/phone/pages/PhoneBroadcastPage.tsx
@@ -1,5 +1,5 @@
 import { lazyWithRetry } from '@/utils/lazyImport'
-const BroadcastPage = lazyWithRetry(() => import('../../pages/broadcast/BroadcastPage'))
+const BroadcastPage = lazyWithRetry(() => import('../../pages/broadcast/BroadcastPage').then(m => ({ default: m.BroadcastPage })))
 
 export default function PhoneBroadcastPage() {
   return <BroadcastPage />
